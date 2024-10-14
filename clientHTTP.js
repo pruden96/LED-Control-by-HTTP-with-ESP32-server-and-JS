dd26dc91f8e35e97762b3c422ef8c3b74c27e58a
pruden96 <carlos.pru11@gmail.com>
2024-10-13 21:07:53 -0400

    Update clientHTTP.js

--- clientHTTP.js
+++ clientHTTP.js
@@ -1,4 +1,4 @@
-const url = 'http://192.168.3.222/led';
+const url = 'http://<ESP32 IP>/led';
 
 const bulbImage = document.getElementById('bombillo');
 const button = document.getElementById('controlButton');
